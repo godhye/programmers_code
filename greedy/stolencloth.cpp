be57530a730f6186d0c9f63548e54dfa07532a02
godhye <79397487+godhye@users.noreply.github.com>
2021-10-09 22:55:41 +0900

    Update stolencloth.cpp
    주의사항좀 읽자 ,,,, ㅂㄷㅂㄷ 30분이면 끝났을거!!!!! 그래도 람다함수랑 캡쳐,,,, for (auto) 배웟다 뿌----듯

--- greedy/stolencloth.cpp
+++ greedy/stolencloth.cpp
@@ -8,29 +8,39 @@ using namespace std;
 int solution(int n, vector<int> lost, vector<int> reserve) {
     int answer = 0;
     answer = n - lost.size();
+     
+    for (auto &v : lost)
+    {
+        //본인이 잃어버린 경우 
+        auto it1 = find(reserve.begin() , reserve.end() , v);
+        if(it1 != reserve.end())
+        {
+         answer++;
+            //본인이 잃어버린경우 lost에서도 삭제해줘야 아래에서 중복으로 안찾음!!
+         v = -100;
+         reserve[it1-reserve.begin()] =  -100;
+        }  
+     }
     
-    //학생 전부다 도둑 맞는 경우 여분 수 반환
-    if(n == lost.size())
-        return reserve.size();
-        
-    //번호 수 정렬 
+    sort(lost.begin() , lost.end());
     sort(reserve.begin() , reserve.end());
         
     for (auto v : lost)
     {
-        auto it = find_if(reserve.begin(), reserve.end(), [=](int a) {
-		//a 값이 v -1 ~ v+1 사이에 있으면 만족 
-		if (a >= v - 1 && a <= v+1)
-			return true;
-		else
-			return false;
-	});
-    if(it!= reserve.end())
-    {
-        answer++;
-        reserve[it-reserve.begin()] =  -100;
         
-    }
+            auto it = find_if(reserve.begin(), reserve.end(), [=](int reservevalue) {
+            //a 값이 v -1 ~ v+1 사이에 있으면 만족    
+		    if (reservevalue == v - 1 || reservevalue == v+1 )
+		    	return true;
+		    else
+			    return false;
+	        });
+            if(it!= reserve.end())
+            {
+             answer++;
+            reserve[it-reserve.begin()] =  -100;
+            }
+       
     }
     return answer;
 }
